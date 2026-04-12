419924c07356ec1afb428164fa28a977bfcb5df3
Vojtěch Jersák <jersavoj@student.cvut.cz>
2026-04-12 14:10:30 +0200

    Added game Launcher

---
+++ src/main/java/cz/cvut/fel/pjv/labyrinthian/Core/Launcher.java
@@ -0,0 +1,9 @@
+package cz.cvut.fel.pjv.labyrinthian.Core;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(GameApplication.class, args);
+    }
+}
